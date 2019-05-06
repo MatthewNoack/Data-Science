35e622842ba32fa3cb6e82f67d5426604509f5c8
MatthewNoack <32495058+MatthewNoack@users.noreply.github.com>
2019-05-06 13:39:04 -0700

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,8 +1,8 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
+from sklearn.datasets import load_iris
 df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.data',
 header=None, sep='\s+')
 df.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS',
@@ -20,3 +20,14 @@ slr = LinearRegression()
 slr.fit(X, y)
 print('Slope: %.3f' % slr.coef_[0])
 print('Intercept: %.3f' % slr.intercept_)
+
+iris = load_iris()
+X = iris.data
+y = iris.target# -*- coding: utf-8 -*-
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
+from sklearn.ensemble import RandomForestClassifier
+rf = RandomForestClassifier(n_estimators=100, n_jobs=-1, random_state=123456)
+rf.fit(X, y)
+importances = rf.feature_importances_
+print(importances)
+print(iris.feature_names)
